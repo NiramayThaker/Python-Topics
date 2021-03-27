6b2950cb4ae1c233fa9b52b027f1786dce2d87d2
Niramay Thaker <70151582+Niramay33@users.noreply.github.com>
2021-03-27 13:18:17 +0530

    Making a turtle walk randomly with rgb

--- turtle_graphics/turtle_random_walk_rgb.py
+++ turtle_graphics/turtle_random_walk_rgb.py
@@ -1,17 +1,26 @@
 import turtle as t
 import random
 
+t.colormode(255)
+
+
+def random_color_finder():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
 tim = t.Turtle()
 tim.shape('turtle')
 
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray",
-           "SeaGreen"]
 directions = [0, 90, 180, 270]
 tim.pensize(15)
 tim.speed('fastest')
 
 for _ in range(200):
-    tim.color(random.choice(colours))
+    tim.color(random_color_finder())
     tim.forward(30)
     tim.setheading(random.choice(directions))
 
