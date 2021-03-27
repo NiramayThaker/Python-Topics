804851036c7b346c41f951a6b40b693fdcfc9847
Niramay Thaker <70151582+Niramay33@users.noreply.github.com>
2021-03-27 13:52:19 +0530

    RGB Spirograph with turtle

--- turtle_graphics/rgb_spirograph.py
+++ turtle_graphics/rgb_spirograph.py
@@ -3,6 +3,9 @@ from turtle import Turtle, Screen
 import random
 
 t.colormode(255)
+tim = Turtle()
+tim.shape('turtle')
+tim.speed('fastest')
 
 
 def random_color_finder():
@@ -13,14 +16,16 @@ def random_color_finder():
     return random_color
 
 
-tim = Turtle()
-tim.shape('turtle')
+def draw_spirograph(size_of_gaps):
+    for _ in range(int(360/size_of_gaps)):
+        tim.color(random_color_finder())
+        tim.circle(100)
+        # tim.left(size_of_gaps)
+        #         or
+        tim.setheading(tim.heading() + size_of_gaps)
 
-for _ in range(80):
-    tim.color(random_color_finder())
-    tim.speed('fastest')
-    tim.circle(100)
-    tim.left(5)
+
+draw_spirograph(5)
 
 my_screen = Screen()
 my_screen.bgcolor('black')
