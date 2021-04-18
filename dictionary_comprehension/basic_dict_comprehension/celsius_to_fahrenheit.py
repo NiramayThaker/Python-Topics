623c1571f364734dda82f0818af09ca79d259f82
Niramay Thaker <70151582+Niramay33@users.noreply.github.com>
2021-04-18 16:24:04 +0530

    Working with dict comprehension for optimization

--- dictionary_comprehension/basic_dict_comprehension/celsius_to_fahrenheit.py
+++ dictionary_comprehension/basic_dict_comprehension/celsius_to_fahrenheit.py
@@ -1,4 +1,5 @@
-# formula to convert Celsius to fahrenheit is (12*9/5)+32
-
 weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24, }
 print({day: (temp_c * 1.8) + 32 for (day, temp_c) in weather_c.items()})
+
+# formula to convert Celsius to fahrenheit is (value_in_Celsius * 9/5) + 32
+
